763c774fbb79e17b24fb52db0d523d96c9de74ef
zurita81 <angel.lara@escola.pr.gov.br>
2026-08-07 09:03:58 -0300

    ok

--- script.js
+++ script.js
@@ -11,6 +11,16 @@ let tamanhoAtualFonte = 100;
 
 //função alto contraste
 
+btnContraste.addEventListener("click,() =>{  
+    
+document.body.classList.toogle("alto-contraste");
+
+//acessibilidade para leitores de tela
+
+const ativo = document.body.classList.contains("alto-contrste")
+btnContraste.setAttribute("aria-pressed", ativo)
+
+)
 
 
 
