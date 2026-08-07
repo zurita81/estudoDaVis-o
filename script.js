02a3ebba9dc58cbd92c36615ce4b028a0efe1ae1
zurita81 <angel.lara@escola.pr.gov.br>
2026-08-07 08:50:22 -0300

    ok

--- script.js
+++ script.js
@@ -1,2 +1,18 @@
 //Botões de acessibilidade
-document.addEventListener
+document.addEventListener("DOMContentLoaded, () => {
+    
+const btnContraste = document.getElementById("btn-contraste");
+const btnAumentar = document.getElementById("btn-aumentar-texto");
+const btnDiminuir = document.getElementById("btn-diminuir-texto")
+
+//aumenta a fonte
+
+let tamanhoAtualFonte = 100;
+
+//função alto contraste
+
+
+
+
+
+}
